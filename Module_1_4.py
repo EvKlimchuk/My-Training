9546936f63412a2c1074235788b8bd1d31b896c3
EvKlimchuk <klimchuk.evgeniya@gmail.com>
2024-09-22 22:47:33 +0300

    Add files via upload

---
+++ Module_1_4.py
@@ -0,0 +1,10 @@
+#Homework "Организация программ и методы строк"
+
+my_string = input("Inpute your text here: ")
+
+print("the number of symbols:", len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace( ' ','' ))
+print(my_string [0])
+print(my_string[-1])
