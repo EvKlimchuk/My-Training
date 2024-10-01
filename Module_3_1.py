1ff73d840de89685ecf158ec3b089e94bed3a7b4
EvKlimchuk <klimchuk.evgeniya@gmail.com>
2024-10-01 22:55:22 +0300

    Add files via upload

---
+++ Module_3_1.py
@@ -0,0 +1,21 @@
+from Module_1_3 import is_student
+
+calls = 0
+def count_calls( ):
+    global calls
+    calls += 1
+
+def string_info(string):
+    count_calls()
+    return len(string), string.upper(), string.lower()
+
+def is_contains (string, list_to_search):
+    count_calls()
+    return string.lower() in [string.lower( ) for string in list_to_search]
+
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban','BaNaN', 'urBan']))
+print(is_contains('cycle', ['recycling','cyclic']))
+print(calls)
