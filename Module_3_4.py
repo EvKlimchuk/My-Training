bdc006dfd379d1b115b6fa438543c3666e55ecd7
EvKlimchuk <klimchuk.evgeniya@gmail.com>
2024-10-06 23:15:07 +0300

    Update Module_3_4.py

--- Module_3_4.py
+++ Module_3_4.py
@@ -4,7 +4,7 @@
 
 def single_root_word(root_word, *other_words):
     same_words= []
-    root_word.lower()
+   
     for word in other_words:
         if root_word.lower() in word.lower():
             same_words.append(word)
