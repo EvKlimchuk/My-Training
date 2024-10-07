99e05ca00ef40baabe93e91051309028f69f4298
EvKlimchuk <klimchuk.evgeniya@gmail.com>
2024-10-07 20:28:57 +0300

    Update Module_3_5.py
    Добавлен вариант для числа, оканчивающегося на 0

--- Module_3_5.py
+++ Module_3_5.py
@@ -1,3 +1,4 @@
+
 # Самостоятельная работа по уроку "Рекурсия"
 # Задача "Рекурсивное умножение цифр"
 
@@ -9,6 +10,28 @@ def get_multiplied_digits(number):
         return first * get_multiplied_digits(int(str_number[1:]))
     else:
         return first
+
+result = get_multiplied_digits(40203)
+print(result)
+
+# Второй вариан. В случае, если введенное число оканчивается на ноль.
+
+def get_multiplied_digits(number):
+    str_number = str(number)
+    first = int(str_number[0])
+    last = int(str_number[-1])
+    if last == 0:
+        str_number = str_number[:-1]
+
+        if len(str_number) > 1:
+            return first * get_multiplied_digits(int(str_number[1:]))
+        else:
+            return first
+    else:
+        if len(str_number) > 1:
+            return first * get_multiplied_digits(int(str_number[1:]))
+        else:
+            return first
 
 result = get_multiplied_digits(40203)
 print(result)
