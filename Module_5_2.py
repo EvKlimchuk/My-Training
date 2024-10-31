547ed84ba74f4c2862467d5f6f4cbcb049b27d04
EvKlimchuk <klimchuk.evgeniya@gmail.com>
2024-10-31 23:07:51 +0300

    Update Module_5_2.py

--- Module_5_2.py
+++ Module_5_2.py
@@ -5,7 +5,7 @@ class House:
     def __len__(self):
         return self.number_of_floors
     def __str__(self):
-        return 'Building name: self.name, number of floors: self.number_of_floors'
+        return f'Building name: {self.name}, number of floors: {self.number_of_floors}'
 
 h3 = House('ЖК Эльбрус', 10)
 h4 = House('ЖК Акация', 20)
